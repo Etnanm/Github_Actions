3288bc745be108f22f4895b3f5582e33cbbb064a
Lexho1 <62320295+Lexho1@users.noreply.github.com>
2020-11-28 23:03:23 +0800

    Update qqyd.js

--- qqyd.js
+++ qqyd.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const download = require('download')
 const smartReplace = require("./smartReplace");
 
+const SEND_KEY = process.env.SEND_KEY
 const $ = new Env('企鹅阅读');
 const notify = $.isNode() ? require('./sendNotify') : '';
 // 公共变量
@@ -17,7 +18,7 @@ const Secrets = {
 
 //下载脚本
 async function downFile() {
-    const url = 'https://raw.githubusercontent.com/ziye12/JavaScript/master/qqread.js'
+    const url = 'https://raw.githubusercontent.com/ZhiYi-N/script/master/qqyds.js'
     await download(url, './', { filename: "temp.js" })
     console.log("下载代码完毕");
 }
@@ -77,13 +78,25 @@ console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 
     }
     var d = new Date(new Date().getTime() + 8 * 60 * 60 * 1000);
     //console.log(d.getHours() +'  '+ d.getMinutes());
-    if (content.includes("今日阅读") && d.getHours()==22 && d.getMinutes()<=20 ) {
+    if (content.includes("今日阅读") && d.getHours()==12 && d.getMinutes()<=20 ) {
         await notify.sendNotify(`${$.name}` + `${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}`, content);
         console.log(content)
     } else {
         //await notify.sendNotify(`${$.name}` + `${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}`, content);
         console.log(content)
     }
+
+    /* if(SEND_KEY) {
+        if (content.includes("Cookie") && d.getHours()==16 && d.getMinutes()<=30) {
+            await notify.sendNotify("企鹅阅读-" + new Date().toLocaleDateString(), content);
+            console.log("企鹅阅读-" + content)
+        }else{
+            console.log("企鹅阅读-" + content)
+        }
+    }else{
+        await notify.sendNotify("企鹅阅读-" + new Date().toLocaleDateString(), content);
+        console.log("企鹅阅读-" + content)
+    }*/
 
     //运行完成后，删除下载的文件
     console.log('运行完成后，删除下载的文件\n')
